2d64ec2795fc74c5c1631686b9b7721bea6d32c1
nataliabaptista <natalia.mendes.baptista@hotmail.com>
2022-07-13 15:47:19 -0300

    Incluindo anotações sobre tipos de dados

---
+++ TipoDados.java
@@ -0,0 +1,49 @@
+public class TipoDados {
+
+
+    public static void main(String[] args) {
+        
+    byte b1 = 10;
+    byte b2 = 30;
+
+    short s1 = 20000;
+    short s2 = 30000;
+
+    int i2 = 200000;
+
+    long l1 = 1000000000000000000l;
+    
+    float f1 = 10.65F; //se colocar so o numero, o java acha que é double.
+
+    double d1 = 10.55;
+    double d2 = 90.89; //não precisa do d
+
+    char c1 = 'A'; //aspas simples e somente um caractere (ou o código unicode de um caractere)
+
+    String st1 = "Hello"; //aspas duplas
+    String st2 = "Hi";
+
+    boolean bo1 = true;
+    boolean bo2 = false;
+
+    
+    System.out.println(b1);
+    System.out.println(b2);
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(i2);
+    System.out.println(l1);
+    System.out.println(f1);
+    System.out.println(d1);
+    System.out.println(d2);
+    System.out.println(c1);
+    System.out.println(st1);
+    System.out.println(st2);
+    System.out.println(bo1);
+    System.out.println(bo2);
+
+
+}
+
+}
+
--- calculadora.java
+++ calculadora.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class calculadora{
+public class Calculadora{
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
